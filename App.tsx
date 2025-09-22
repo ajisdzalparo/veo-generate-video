d22d3820d4d5aa570da891c977a69adaf1e6c51e
Ajis Dzalparo <ajisdzalparo22@gmail.com>
2025-09-22 12:53:02 +0700

    add footer

--- App.tsx
+++ App.tsx
@@ -214,6 +214,7 @@ const App: React.FC = () => {
             )}
           </div>
         </main>
+        <footer className="mt-10 text-center text-gray-500 text-sm">Developed by Ajis Dzalparo</footer>
       </div>
     </div>
   );
